11227568b4c3a896b1d20e36fc797f571c19200a
nour-abolila <nouraliabolilanaaltya101@gmail.com>
2026-08-08 19:35:40 +0300

    Implement cart functionality with add, remove, and retrieval methods; add validation request for adding items to cart

--- app/Http/Controllers/Cart/CartController.php
+++ app/Http/Controllers/Cart/CartController.php
@@ -3,9 +3,34 @@
 namespace App\Http\Controllers\Cart;
 
 use App\Http\Controllers\Controller;
+use App\Services\CartService;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    //
+    public function __construct(protected CartService $cartService) {}
+
+
+    public function show()
+    {
+        $cart = $this->cartService->getCart(auth()->user());
+
+        return success('Cart retrieved successfully', new CartResource($cart));
+    }
+
+
+    public function add(AddToCartRequest $request)
+    {
+        $cart = $this->cartService->addToCart(auth()->user(), $request->validated());
+
+        return success('Product added to cart', new CartResource($cart->load('items.product')));
+    }
+
+
+    public function remove($productId)
+    {
+        $this->cartService->removeFromCart(auth()->user(), $productId);
+
+        return success('Product removed successfully');
+    }
 }
