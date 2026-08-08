c66d7cfe02550c9012162914b89dac75cb38edcd
nour-abolila <nouraliabolilanaaltya101@gmail.com>
2026-08-08 19:35:04 +0300

    Add Cart and CartItem models, and implement cart routes

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -34,4 +34,9 @@ class Product extends Model
     {
         return $this->hasMany(Review::class);
     }
+
+    public function cartItems()
+    {
+        return $this->hasMany(CartItem::class);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,13 @@ use App\Http\Controllers\Api\Home\HomeController;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\SocialiteController;
+use App\Http\Controllers\Cart\CartController;
 use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\Product\CategoryController;
 use App\Http\Controllers\Product\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 
 
@@ -57,3 +59,15 @@ Route::prefix('products')->group(function () {
 
 
 Route::post('/orders/{order}/checkout', [PaymentController::class, 'pay']);
+
+
+// Cart Routes
+
+Route::middleware('auth:api')->prefix('cart')->controller(CartController::class)->group(function () {
+
+    Route::get('/', 'show');
+
+    Route::post('/add', 'add');
+
+    Route::delete('/remove/{productId}', 'remove');
+});
